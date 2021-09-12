d3b44dc2d0d160920e6d3a9f7e461b3b67a640a9
Krsikapa00 <krsinich589@gmail.com>
2021-09-12 16:15:45 -0400

    altered stepper component to be responsive

--- src/pages/speaker.js
+++ src/pages/speaker.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import StepperFrame from '../components/common/stepper/StepperFrame'
 import StepperCardGeneral from '../components/common/stepper/StepperCardGeneral'
+import AhelyS from '../assets/images/speakers/headshots/AhelyS.jpg'
+import AndreaCorey from '../assets/images/speakers/headshots/AndreaCorey.jpg'
 
-const SpeakerCards = {
-    0: [<StepperCardGeneral number = {1}/>, <StepperCardGeneral number = {2}/>, <StepperCardGeneral number = {3}/>,
-        <StepperCardGeneral number = {4}/>, <StepperCardGeneral number = {5}/>, <StepperCardGeneral number = {6}/>],
-    1: [<StepperCardGeneral number = {7}/>, <StepperCardGeneral number = {8}/>, <StepperCardGeneral number = {9}/>,
-        <StepperCardGeneral number = {10}/>, <StepperCardGeneral number = {11}/>],
-    2: [<StepperCardGeneral number = {12}/>, <StepperCardGeneral number = {13}/>, <StepperCardGeneral number = {14}/>,
-        <StepperCardGeneral number = {15}/>],
-    3: [<StepperCardGeneral number = {16}/>, <StepperCardGeneral number = {17}/>, <StepperCardGeneral number = {18}/>],
-    4: [<StepperCardGeneral number = {19}/>, <StepperCardGeneral number = {20}/>],
-    5: [<StepperCardGeneral number = {21}/>],
-};
+import Rogers from '../assets/images/sponsors/Rogers.png'
+
+
+const SpeakerCards = [
+     [
+        {picture: AhelyS,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers},
+        {picture: AhelyS,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers},
+        {picture: AhelyS,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers},
+        {picture: AhelyS,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers},
+        {picture: AhelyS,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers}
+
+    ],
+    [
+        {picture: AndreaCorey,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers},
+        
+        {picture: AhelyS,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers},
+        {picture: AhelyS,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers},
+        {picture: AhelyS,  name:"Nicholas Krsikapa", position:"Software Developer", company:Rogers}
+    ]
+];
 
 const Speaker = () => {
     return (
         <div>
-            <h1>Speakers</h1>
             <StepperFrame cards = {SpeakerCards}/>
         </div>
     )
