0a148af0fac4fe10cb68adf1bc9bcd2fc656aa67
Carter Conboy <conboy2001@gmail.com>
2021-09-30 20:58:30 -0400

    Added events dates, grey frosting and reduced coming soon image side margins

--- src/components/schedule/ComingSoon.js
+++ src/components/schedule/ComingSoon.js
@@ -7,8 +7,11 @@ export default function ComingSoon() {
             <h1 class="title">Event Schedule 2022</h1>
             <div class="imageBox">
                 <h1 class="text">Coming Soon...</h1>
+                <br></br>
+                <h2 class="text">Event Dates: January 28th, 29th, and 30th, 2022</h2>
             </div>
             <h1 class="text2">Coming Soon...</h1>
+            <h2 class="text2">Event Dates: January 28th, 29th, and 30th, 2022</h2>
         </div>
     ); 
 }
